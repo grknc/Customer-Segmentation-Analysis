c1cd3106413b2ba34b5aeff6494b46115ee4f531
grknc <m.gurkan.canakci@gmail.com>
2024-12-06 22:39:59 +0300

    eda.py güncellendi

--- src/eda.py
+++ src/eda.py
@@ -8,7 +8,6 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 import numpy as np
 import pandas as pd
 import seaborn as sns
-import random
 from matplotlib import pyplot as plt
 import random
 # !pip install missingno
